ac870094bd64fe06d32e94722fff357ccd5cac2e
ShajithaNasrin <shajitha.msn@gmail.com>
2016-10-15 14:28:33 +0530

    Add files via upload

---
+++ Expression.java
@@ -0,0 +1,17 @@
+class Expression
+{
+	public static void main(String[] args) 
+	{
+ 
+		int a=10;
+		int b=20;
+		b=a-- - --a;
+		int c= a--;
+		int d=a>>2;
+		int e = a & b;
+		System.out.println("b variable" +" "+ b);
+		System.out.println("c variable" +" "+ c );
+		System.out.println("d variable" +" "+ d);
+		System.out.println("e variable" +" "+e);
+	}
+}
